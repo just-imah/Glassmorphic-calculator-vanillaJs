6cc00e108f60f6635c91de2d53a116dc0d0ffce3
just-imah <ima.ntuen47@gmail.com>
2021-03-29 10:55:11 +0100

    second attempt to calc

--- script.js
+++ script.js
@@ -1,25 +1,81 @@
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+};
+// console.log(calculator)
 
-let buttons = document.querySelectorAll('.btn');
-let screen = document.getElementById("working");
-let clear = document.getElementById("clear");
-let remove = document.getElementById("backspace")
-let add = document.getElementById("plus")
-let equals = document.getElementById("data-equals")
+// update display
+function updateDisplay() {
+    // select the element with id of the working screen
+    const display = document.getElementById('working-screen')
+    // update the value of the element with the content of displayValue
+    display.value = calculator.displayValue;
+}
+updateDisplay()
 
-buttons.forEach(el => {
-    el.addEventListener("click", (e) => {
-       
-        // console.log('button', e.target.id)
-        screen.innerHTML += e.target.id;
-        // console.log(buttons.length)
-        if (e.target.id.includes('.')) {
-        //    return = true
-         }
-    });
-}) 
-clear.addEventListener("click", (e) => {
-    screen.innerHTML = '';
+// handling the presses
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener("click", (event) => {
+    const { target } = event;
+
+    if (!target.matches('buttton')) {
+        return;
+    }
+    if (!target.classList.contains('operator')) {
+        console.log('operator', target.value)
+    }
+    //console.log('keys')
 })
+
+
+
+
+
+
+
+
+
+
+
+
+// let buttons = document.querySelectorAll('.btn');
+// let screen = document.getElementById("working-screen");
+// let clear = document.getElementById("clear");
+// let remove = document.getElementById("backspace")
+// let add = document.getElementById("plus")
+// let equals = document.getElementById("data-equals")
+
+// buttons.forEach(el => {
+//     el.addEventListener("click", (e) => {
+       
+//         // console.log('button', e.target.id)
+//         screen.innerHTML += e.target.id;
+//         // console.log(buttons.length)
+//         if (e.target.id.includes('.')) {
+//         //    return = true
+//          }
+//     });
+// }) 
+// clear.addEventListener("click", (e) => {
+//     screen.innerHTML = '';
+// })
+
+// remove.addEventListener("click", (e) => {
+//     screen.innerHTML = e.target.id.slice(0, -1)
+// })
+
+
+
+
+
+
+
+
+
+
+
 // remove.addEventListener("click", (e) => {
 //     // screen.innerHTML.slice(0, -1)
 //     // console.log(screen.value)
@@ -28,6 +84,25 @@ clear.addEventListener("click", (e) => {
 //     // screen.innerHTML = remove;
 //     screen.innerHTML = e.target.id.slice(0, -1)
 // })
-remove.addEventListener("click", (e) => {
-    screen.innerHTML = e.target.id.slice(0, -1)
-})
+
+
+
+
+
+// const calculator = {
+//     screenValue: '0',
+//     firstOperand : null,
+//     operator: null,
+//     waitingForSecondOperand: false
+// };
+
+// function handleOperator(nextOperator) {
+//     let { firstOperand, screenValue, operator } = calculator
+//     let inputValue = parseFloat(screenValue);
+
+//     if (operator && calculator.waitingForSecondOperand) {
+//         calculator.operator = nextOperator;
+//         console.log(calculator);
+//         return;
+//     }
+// }
